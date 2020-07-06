f623f03ff36eb5e28560bc69dc60d06f0d4e1e1e
Christian Gonzalez <christian.gonzalez.2633@gmail.com>
2020-07-05 21:49:12 -0500

    added JAWSDB_URL if-else statement for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
 // Dependencies
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "amillionwords",
-    database: "burgers_db"
-});
+const connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "amillionwords",
+        database: "burgers_db"
+    });
+}
+
+
 
 connection.connect(function (err) {
     if (err) {
